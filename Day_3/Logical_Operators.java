f3b2c59df314664d80b9dbb1746f3b33e4b82490
Chitranshu <chitranshu2411@gmail.com>
2026-04-02 20:32:36 +0530

    Covered & practice Logical Operator
    Covered & practice Logical Operator

---
+++ Day_3/Logical_Operators.java
@@ -0,0 +1,10 @@
+public class Logical_Operators {
+    public static void main(String[] args) {
+        boolean completedDsa = true;
+        boolean completedCore = false;
+
+        System.out.println(completedDsa && completedCore);
+        System.out.println(completedDsa || completedCore);
+        System.out.println(!completedCore);
+    }
+}
