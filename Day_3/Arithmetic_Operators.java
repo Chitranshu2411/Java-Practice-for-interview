51215bb9934a0c9d37407a8ac4bcdd2f8d0245f0
Chitranshu <chitranshu2411@gmail.com>
2026-04-02 13:48:54 +0530

    Implement arithmetic operations
    Implement arithmetic operations

---
+++ Day_3/Arithmetic_Operators.java
@@ -0,0 +1,18 @@
+public class Arithmetic_Operators {
+    public static void main(String[] args) {
+        int solvedThisWeek = 25;
+        int solvedLastWeek = 15;
+
+        int total = solvedThisWeek + solvedLastWeek;
+        int difference = solvedThisWeek - solvedLastWeek;
+        int projected = solvedThisWeek * 4;
+        int average = solvedThisWeek / 7;
+        int remainder = solvedThisWeek % 7;
+
+        System.out.println(total);
+        System.out.println(difference);
+        System.out.println(projected);
+        System.out.println(average);
+        System.out.println(remainder);
+    }
+}
