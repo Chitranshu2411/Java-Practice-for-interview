1b3f29e59afae6374ff98ad84f38b4d9ea378c25
Chitranshu <chitranshu2411@gmail.com>
2026-04-05 23:47:18 +0530

    Implemented for loop
    Implemented for loop

---
+++ Day_5/For_loop.java
@@ -0,0 +1,15 @@
+public class For_loop {
+    public static void main(String[] args) {
+        for ( int i = 1; i<=5 ; i++){
+            System.out.println("value of i :" + i);
+        }
+
+        for ( int i = 1; i<=5; i++){
+            System.out.println("Chitranshu");
+        }
+
+        for (int i = 1; i<=10; i= i+2){
+            System.out.println(i);
+        }
+    }
+}
