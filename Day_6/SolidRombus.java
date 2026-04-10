0a0f00ba879e6a39a04723322f82dbfcaacf5e20
Chitranshu <chitranshu2411@gmail.com>
2026-04-10 21:45:04 +0530

    SolidRombus Practice
     SolidRombus Practice

---
+++ Day_6/SolidRombus.java
@@ -0,0 +1,21 @@
+public class SolidRombus {
+    public static void main(String[] args) {
+        int n = 5;
+
+        for (int i = 1; i <= n; i++) {
+            //for each row -> spaces , stars
+
+            //space
+            for (int j = 1; j <= n - i; j++) {
+                System.out.print(" ");
+            }
+
+            //stars
+            for (int j = 1; j <= n; j++) {
+                System.out.print("* ");
+            }
+            //move to next line
+            System.out.println();
+        }
+    }
+}
