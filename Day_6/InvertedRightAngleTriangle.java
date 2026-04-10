473f3c0b500e13ee9a4a121252b15e2caf21dfe6
Chitranshu <chitranshu2411@gmail.com>
2026-04-10 22:05:37 +0530

    InvertedRightAngleTriangle Implementation
    InvertedRightAngleTriangle Implementation

---
+++ Day_6/InvertedRightAngleTriangle.java
@@ -0,0 +1,14 @@
+public class InvertedRightAngleTriangle {
+    public static void main(String[] args) {
+        int n = 5;
+
+        for(int i=1; i<=n ; i++){
+            //for each row -> variable columns
+            for (int j=1; j<=n-i+1; j++){
+                System.out.print("* ");
+            }
+            //move to next row
+            System.out.println("");
+        }
+    }
+}
