900c0c5547d0d9bec32cb4467eb58a1c49f42b01
Chitranshu <chitranshu2411@gmail.com>
2026-04-24 13:43:07 +0530

    Add main class for executing string practice programs
    Add main class for executing string practice programs

---
+++ Day_7/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        int [] arr = { 2,3,4,5,6,13};
+        int a = 10;
+        String name = "Chitranshu Yadav";
+        System.out.println(name);
+    }
+}
