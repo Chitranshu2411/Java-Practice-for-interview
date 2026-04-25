33a7f1de1acd7061801a6740c942ca8b07863208
Chitranshu <chitranshu2411@gmail.com>
2026-04-25 22:04:21 +0530

    Main class to test

---
+++ Day_8/Main.java
@@ -0,0 +1,11 @@
+package Day_8;
+
+public class Main {
+    public static void main(String[] args) {
+        //write a function that prints hello world
+        message();
+    }
+    static void message(){
+        System.out.println("Hello World");
+    }
+}
